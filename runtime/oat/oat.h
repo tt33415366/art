d8261f0d75a338c4b74bac006ae7a884f88b1e02
Steven Moreland <smoreland@google.com>
2024-12-11 01:46:52 +0000

    Revert "Force 4K ELF alignment on art/odex files."
    This reverts commit fcb5c3a0cc8776cfedf8d289c5de19b605afe7ae.
    
    Reason for revert: getting ready to reland, as fixes are in place:
    - b/376814207#comment91
    
    This is not a clean revert, only due to oat.h version requirement.
    
    Test: TH, is revert
    Bug: 376814207
    Bug: 378792349
    
    Change-Id: Ieb9672ba07149bfd2c4b20c3afa17eb19f663c6b

--- runtime/oat/oat.h
+++ runtime/oat/oat.h
@@ -44,7 +44,7 @@ std::ostream& operator<<(std::ostream& stream, StubType stub_type);
 class EXPORT PACKED(4) OatHeader {
  public:
   static constexpr std::array<uint8_t, 4> kOatMagic { { 'o', 'a', 't', '\n' } };
-  // Last oat version changed reason: move dynamic sections to start of OAT file.
+  // Last oat version changed reason: Restore to 16 KB ELF alignment.
   static constexpr std::array<uint8_t, 4> kOatVersion{{'2', '5', '6', '\0'}};
 
   static constexpr const char* kDex2OatCmdLineKey = "dex2oat-cmdline";
