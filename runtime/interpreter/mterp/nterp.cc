22b0de60557ec67b8c10ca41e95f3f03bd2abd2f
Liana Kazanova <lkazanova@google.com>
2024-12-12 10:03:34 -0800

    Revert "Reduce hotness on interpreter lookups."
    This reverts commit 7dcc228bc873a9141e86d2f5f5a54c1aef5be073.
    
    Reason for revert: DroidMonitor: Reverting due to http://b/383373141 ( go/jank-rotation-playbook)
    
    Change-Id: I9c092f13d125502f4eef43c61309a9dcaacf1db1

--- runtime/interpreter/mterp/nterp.cc
+++ runtime/interpreter/mterp/nterp.cc
@@ -119,7 +119,11 @@ void CheckNterpAsmConstants() {
 inline void UpdateHotness(ArtMethod* method) REQUIRES_SHARED(Locks::mutator_lock_) {
   // The hotness we will add to a method when we perform a
   // field/method/class/string lookup.
-  method->UpdateCounter(0xf);
+  Runtime* runtime = Runtime::Current();
+  bool increase_hotness_for_ui = runtime->GetStartupCompleted() &&
+      runtime->InJankPerceptibleProcessState() &&
+      Thread::Current()->IsJitSensitiveThread();
+  method->UpdateCounter(increase_hotness_for_ui ? 0x6ff : 0xf);
 }
 
 template<typename T>
