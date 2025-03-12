b2d23dd55d0f1d16beb43a247d62bf09a8060665
Jiakai Zhang <jiakaiz@google.com>
2025-03-12 16:44:02 +0000

    Add helper classes for reading and writing SDC files.
    Secure dex metadata companion (SDC) file is a file type that augments a
    secure dex metadata (SDM) file with additional metadata. It is generated
    on device when the SDM file is being installed.
    
    Bug: 377474232
    Test: m test-art-host-gtest-art_runtime_tests
    Change-Id: I13cf86db9cdcca85e5b59bd992a4deddeeb57501

---
+++ runtime/oat/sdc_file.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ART_RUNTIME_OAT_SDC_FILE_H_
+#define ART_RUNTIME_OAT_SDC_FILE_H_
+
+#include <ctime>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <utility>
+
+#include "base/macros.h"
+#include "base/os.h"
+
+namespace art HIDDEN {
+
+// A helper class to read a secure dex metadata companion (SDC) file.
+//
+// Secure dex metadata companion (SDC) file is a file type that augments a secure dex metadata (SDM)
+// file with additional metadata.
+//
+// 1. There may be exactly one SDC file accompanying each SDM file. An SDC file without a
+//    corresponding SDM file, or with a mismatching SDM timestamp, is garbage.
+// 2. They are always local on device.
+// 3. They are only read and written by the ART module.
+// 4. A later version of the ART module must be able to understand the contents.
+//
+// It is a text file in the format of:
+//   key1=value1\n
+//   key2=value2\n
+//   ...
+// Repeated keys are not allowed. This is an extensible format, so versioning is not needed.
+class SdcReader {
+ public:
+  static std::unique_ptr<SdcReader> Load(const std::string filename, std::string* error_msg);
+
+  // The mtime of the SDM file on device.
+  // This is for detecting obsolete SDC files.
+  time_t GetSdmTimestamp() const { return sdm_timestamp_; }
+
+  // The value of `Runtime::GetApexVersions` at the time where the SDM file was first seen on
+  // device. This is for detecting samegrade placebos.
+  std::string_view GetApexVersions() const { return apex_versions_; }
+
+ private:
+  SdcReader() = default;
+
+  std::string content_;
+  time_t sdm_timestamp_;
+  std::string_view apex_versions_;
+};
+
+// A helper class to write a secure dex metadata companion (SDC) file.
+class EXPORT SdcWriter {
+ public:
+  // Takes ownership of the file.
+  explicit SdcWriter(File&& file) : file_(std::move(file)) {}
+
+  // See `SdcReader::GetSdmTimestamp`.
+  void SetSdmTimestamp(time_t value) { sdm_timestamp_ = value; }
+
+  // See `SdcReader::GetApexVersions`.
+  void SetApexVersions(std::string_view value) { apex_versions_ = value; }
+
+  bool Save(std::string* error_msg);
+
+ private:
+  File file_;
+  time_t sdm_timestamp_ = 0;
+  std::string apex_versions_;
+};
+
+}  // namespace art
+
+#endif  // ART_RUNTIME_OAT_SDC_FILE_H_
